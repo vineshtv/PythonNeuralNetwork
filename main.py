dcc1f4e7f84f5b65f7b05095952c89c296a9619c
vineshtv <tv.vinesh@gmail.com>
2018-02-10 02:05:14 +0530

    WIP - Added query function for the NN

---
+++ main.py
@@ -0,0 +1,24 @@
+from neural_network import NeuralNetwork 
+
+def main():
+    # number of input, hidden and output nodes
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+
+    # Learning rate = 0.3
+    learning_rate = 0.3
+
+    # Create an instance of neural network
+    nn = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+    inputs = [1.0, 0.5, -1.5]
+
+    # query the neural network
+    output = nn.query(inputs)
+
+    print ("output is : ", output)
+
+
+if __name__ == "__main__":
+    main()
--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,6 @@
 """ This module contains the class definition for the neural network """
 import numpy as np
+import scipy.special
 
 class NeuralNetwork(object):
     ''' NeuralNetwork class definition '''
@@ -18,10 +19,27 @@ class NeuralNetwork(object):
         self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
         self.who = np.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
 
+        # activation function here is the sigmoid function 
+        # expit is the inbuilt sigmoid function in scipy
+        self.activation_function = lambda x: scipy.special.expit(x)
+
     def train(self):
         ''' This function trains the neural network '''
         pass
 
-    def query(self):
+    def query(self, inputs_list):
         ''' Query the neural network '''
-        pass
+        # convert inputs list into 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
+
+        # calculate signals into hidden layers
+        hidden_inputs = np.dot(self.wih, inputs)
+        # calculate the signals emerging from the hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate the signals into the final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from the final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
