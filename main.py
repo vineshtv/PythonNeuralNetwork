39e96f8415178411c8ce369ed465915ad3aa64c7
vineshtv <tv.vinesh@gmail.com>
2018-02-13 23:48:38 +0530

    Loaded the MNIST dataset

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
+
+from sklearn.datasets import fetch_mldata
 from neural_network import NeuralNetwork 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     # number of input, hidden and output nodes
-    input_nodes = 3
-    hidden_nodes = 3
-    output_nodes = 3
+    input_nodes = 784
+    hidden_nodes = 100
+    output_nodes = 10
 
     # Learning rate = 0.3
     learning_rate = 0.3
@@ -12,12 +18,36 @@ def main():
     # Create an instance of neural network
     nn = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 
-    inputs = [1.0, 0.5, -1.5]
-    
-    # query the neural network
-    output = nn.query(inputs)
+    mnist = fetch_mldata('MNIST original')
+    X, y = mnist["data"], mnist["target"]
 
-    print ("output is : ", output)
+    X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+   
+    shuffle_index = np.random.permutation(60000)
+    X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+    
+    # test the neural network
+
+    # scorecard for how well the network performs
+    scorecard = []
+
+    index = 0;
+    for record in X_train:
+        scaled_input = (np.asfarray(record) / 255.0 * 0.99) + 0.01
+        correct_label = y_train[index]
+        index = index + 1
+        outputs = nn.query(scaled_input)
+        label = np.argmax(outputs)
+
+        #append correct or incorrect to list
+        if(label == correct_label):
+            scorecard.append(1)
+        else:
+            scorecard.append(0)
+
+    #calulate the performance
+    scorecard_array = np.asarray(scorecard)
+    print("Performance = ", scorecard_array.sum() / scorecard_array.size)
 
 if __name__ == "__main__":
     main()
